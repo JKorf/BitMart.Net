31edd15046d5b9dcb38ec6c375d0c6ca102d4d9b
Jkorf <jankorf91@gmail.com>
2025-01-07 14:27:45 +0100

    Updated CryptoExchange.Net version, added Type property to BitMartExchange class

--- BitMart.Net/BitMartExchange.cs
+++ BitMart.Net/BitMartExchange.cs
@@ -43,6 +43,11 @@ namespace BitMart.Net
             };
 
         /// <summary>
+        /// Type of exchange
+        /// </summary>
+        public static ExchangeType Type { get; } = ExchangeType.CEX;
+
+        /// <summary>
         /// Format a base and quote asset to a BitMart recognized symbol 
         /// </summary>
         /// <param name="baseAsset">Base asset</param>
