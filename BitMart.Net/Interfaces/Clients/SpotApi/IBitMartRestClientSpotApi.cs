fb52d0953f05cc30c14f84675cb4b290531b0b25
Jkorf <jankorf91@gmail.com>
2025-04-08 16:16:08 +0200

    wip

--- BitMart.Net/Interfaces/Clients/SpotApi/IBitMartRestClientSpotApi.cs
+++ BitMart.Net/Interfaces/Clients/SpotApi/IBitMartRestClientSpotApi.cs
@@ -11,26 +11,31 @@ namespace BitMart.Net.Interfaces.Clients.SpotApi
         /// <summary>
         /// Endpoints related to account settings, info or actions
         /// </summary>
+        /// <see cref="IBitMartRestClientSpotApiAccount"/>
         public IBitMartRestClientSpotApiAccount Account { get; }
 
         /// <summary>
         /// Endpoints related to retrieving market and system data
         /// </summary>
+        /// <see cref="IBitMartRestClientSpotApiExchangeData"/>
         public IBitMartRestClientSpotApiExchangeData ExchangeData { get; }
 
         /// <summary>
         /// Endpoints related to margin borrow and repayment
         /// </summary>
+        /// <see cref="IBitMartRestClientSpotApiMargin"/>
         public IBitMartRestClientSpotApiMargin Margin { get; }
 
         /// <summary>
         /// Endpoints related to sub account management
         /// </summary>
+        /// <see cref="IBitMartRestClientSpotApiSubAccount"/>
         public IBitMartRestClientSpotApiSubAccount SubAccount { get; }
 
         /// <summary>
         /// Endpoints related to orders and trades
         /// </summary>
+        /// <see cref="IBitMartRestClientSpotApiTrading"/>
         public IBitMartRestClientSpotApiTrading Trading { get; }
 
         /// <summary>
--- BitMart.Net/Interfaces/Clients/UsdFuturesApi/IBitMartRestClientUsdFuturesApi.cs
+++ BitMart.Net/Interfaces/Clients/UsdFuturesApi/IBitMartRestClientUsdFuturesApi.cs
@@ -12,25 +12,29 @@ namespace BitMart.Net.Interfaces.Clients.UsdFuturesApi
         /// <summary>
         /// Endpoints related to account settings, info or actions
         /// </summary>
+        /// <see cref="IBitMartRestClientUsdFuturesApiAccount"/>
         public IBitMartRestClientUsdFuturesApiAccount Account { get; }
 
         /// <summary>
         /// Endpoints related to sub account management
         /// </summary>
+        /// <see cref="IBitMartRestClientUsdFuturesApiSubAccount"/>
         public IBitMartRestClientUsdFuturesApiSubAccount SubAccount { get; }
 
         /// <summary>
         /// Endpoints related to retrieving market and system data
+        /// <see cref="IBitMartRestClientUsdFuturesApiExchangeData"/>
         /// </summary>
         public IBitMartRestClientUsdFuturesApiExchangeData ExchangeData { get; }
 
         /// <summary>
         /// Endpoints related to orders and trades
         /// </summary>
+        /// <see cref="IBitMartRestClientUsdFuturesApiTrading"/>
         public IBitMartRestClientUsdFuturesApiTrading Trading { get; }
 
         /// <summary>
-        /// Get the shared rest requests client. This interface is shared with other exhanges to allow for a common implementation for different exchanges.
+        /// Get the shared rest requests client. This interface is shared with other exchanges to allow for a common implementation for different exchanges.
         /// </summary>
         public IBitMartRestClientUsdFuturesApiShared SharedClient { get; }
     }
