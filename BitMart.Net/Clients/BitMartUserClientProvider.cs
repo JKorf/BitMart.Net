e198ae126863d61740661406063bf78f4fd4b570
Jkorf <jankorf91@gmail.com>
2025-08-22 14:54:00 +0200

    Added ClearUserClients method to user client provider

--- BitMart.Net/Clients/BitMartUserClientProvider.cs
+++ BitMart.Net/Clients/BitMartUserClientProvider.cs
@@ -54,6 +54,13 @@ namespace BitMart.Net.Clients
         }
 
         /// <inheritdoc />
+        public void ClearUserClients(string userIdentifier)
+        {
+            _restClients.TryRemove(userIdentifier, out _);
+            _socketClients.TryRemove(userIdentifier, out _);
+        }
+
+        /// <inheritdoc />
         public IBitMartRestClient GetRestClient(string userIdentifier, ApiCredentials? credentials = null, BitMartEnvironment? environment = null)
         {
             if (!_restClients.TryGetValue(userIdentifier, out var client))
