d030a71bb86db729c997fda4352f915cc80a43bb
Jkorf <jankorf91@gmail.com>
2026-04-28 10:04:46 +0200

    Added OpenInterest24H, OpenInterestValue24H properties to BitMartOpenInterest model

--- BitMart.Net/Objects/Models/BitMartOpenInterest.cs
+++ BitMart.Net/Objects/Models/BitMartOpenInterest.cs
@@ -30,6 +30,16 @@ namespace BitMart.Net.Objects.Models
         /// </summary>
         [JsonPropertyName("open_interest_value")]
         public decimal OpenInterestValue { get; set; }
+        /// <summary>
+        /// ["<c>open_interest</c>"] Open interest 24h
+        /// </summary>
+        [JsonPropertyName("open_interest_24h")]
+        public decimal OpenInterest24H { get; set; }
+        /// <summary>
+        /// ["<c>open_interest_value</c>"] Open interest value 24h
+        /// </summary>
+        [JsonPropertyName("open_interest_value_24h")]
+        public decimal OpenInterestValue24H { get; set; }
     }
 
 
