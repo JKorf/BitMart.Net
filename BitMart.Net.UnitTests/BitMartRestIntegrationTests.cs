fab5780b0d3f8e5dc73e3cabdbc9dd0a58c1e2fe
Jkorf <jankorf91@gmail.com>
2026-07-01 15:16:02 +0200

    Updated CryptoExchange.Net to V12.0.2, Updated integration tests to new warning support

--- BitMart.Net.UnitTests/BitMartRestIntegrationTests.cs
+++ BitMart.Net.UnitTests/BitMartRestIntegrationTests.cs
@@ -55,12 +55,15 @@ namespace BitMart.Net.UnitTests
         [Test]
         public async Task TestSpotAccount()
         {
-            await RunAndCheckResult(client => client.SpotApi.Account.GetFundingBalancesAsync(default, default, default), true, true, "data.wallet");
-            await RunAndCheckResult(client => client.SpotApi.Account.GetSpotBalancesAsync(default), true, true, "data.wallet");
-            await RunAndCheckResult(client => client.SpotApi.Account.GetWithdrawalQuotaAsync("ETH", default), true, true, "data");
-            await RunAndCheckResult(client => client.SpotApi.Account.GetBaseTradeFeesAsync(default), true, true, "data");
-            await RunAndCheckResult(client => client.SpotApi.Account.GetSymbolTradeFeeAsync("ETH_USDT", default), true, true, "data");
-            
+            var warnings = new List<Exception>();
+            await RunAndCheckResult(warnings, client => client.SpotApi.Account.GetFundingBalancesAsync(default, default, default), true, "data.wallet");
+            await RunAndCheckResult(warnings, client => client.SpotApi.Account.GetSpotBalancesAsync(default), true, "data.wallet");
+            await RunAndCheckResult(warnings, client => client.SpotApi.Account.GetWithdrawalQuotaAsync("ETH", default), true, "data");
+            await RunAndCheckResult(warnings, client => client.SpotApi.Account.GetBaseTradeFeesAsync(default), true, "data");
+            await RunAndCheckResult(warnings, client => client.SpotApi.Account.GetSymbolTradeFeeAsync("ETH_USDT", default), true, "data");
+            foreach (var warning in warnings)
+                Assert.Warn(warning.Message);
+
             // Needs margin account
             //await RunAndCheckResult(client => client.SpotApi.Account.GetIsolatedMarginAccountsAsync(default, default), true);
         }
@@ -68,62 +71,80 @@ namespace BitMart.Net.UnitTests
         [Test]
         public async Task TestSpotMargin()
         {
-            await RunAndCheckResult(client => client.SpotApi.Margin.GetBorrowHistoryAsync("ETH_USDT", default, default, default, default, default), true, true, "data.records");
-            await RunAndCheckResult(client => client.SpotApi.Margin.GetRepayHistoryAsync("ETH_USDT", default, default, default, default, default, default), true, true, "data.records");
-            await RunAndCheckResult(client => client.SpotApi.Margin.GetBorrowInfoAsync(default, default), true, true, "data.symbols");
+            var warnings = new List<Exception>();
+            await RunAndCheckResult(warnings, client => client.SpotApi.Margin.GetBorrowHistoryAsync("ETH_USDT", default, default, default, default, default), true, "data.records");
+            await RunAndCheckResult(warnings, client => client.SpotApi.Margin.GetRepayHistoryAsync("ETH_USDT", default, default, default, default, default, default), true, "data.records");
+            await RunAndCheckResult(warnings, client => client.SpotApi.Margin.GetBorrowInfoAsync(default, default), true, "data.symbols");
+            foreach (var warning in warnings)
+                Assert.Warn(warning.Message);
         }
 
         [Test]
         public async Task TestSpotExchangeData()
         {
+            var warnings = new List<Exception>();
             await RunAndCheckResult(client => client.SpotApi.ExchangeData.GetServerTimeAsync(default), false);
-            await RunAndCheckResult(client => client.SpotApi.ExchangeData.GetAssetsAsync(default), false, true, "data.currencies");
-            await RunAndCheckResult(client => client.SpotApi.ExchangeData.GetSymbolsAsync(default), false, true, "data.symbols");
-            await RunAndCheckResult(client => client.SpotApi.ExchangeData.GetSymbolNamesAsync(default), false, true, "data.symbols");
-            await RunAndCheckResult(client => client.SpotApi.ExchangeData.GetTickerAsync("ETH_USDT", default), false, true, "data");
-            await RunAndCheckResult(client => client.SpotApi.ExchangeData.GetTickersAsync(default), false, true, "data");
-            await RunAndCheckResult(client => client.SpotApi.ExchangeData.GetAssetDepositWithdrawInfoAsync(default, default), false, true, "data.currencies");
-            await RunAndCheckResult(client => client.SpotApi.ExchangeData.GetServerStatusAsync(default), false, true, "data.service");
-            await RunAndCheckResult(client => client.SpotApi.ExchangeData.GetKlinesAsync("ETH_USDT", Enums.KlineInterval.OneDay, default, default, default, default), false, true, "data");
-            await RunAndCheckResult(client => client.SpotApi.ExchangeData.GetKlineHistoryAsync("ETH_USDT", Enums.KlineInterval.OneDay, default, default, default, default), false, true, "data");
-            await RunAndCheckResult(client => client.SpotApi.ExchangeData.GetTradesAsync("ETH_USDT", default, default), false, true, "data");
-            await RunAndCheckResult(client => client.SpotApi.ExchangeData.GetOrderBookAsync("ETH_USDT", default, default), false, true, "data");
+            await RunAndCheckResult(warnings, client => client.SpotApi.ExchangeData.GetAssetsAsync(default), false, "data.currencies");
+            await RunAndCheckResult(warnings, client => client.SpotApi.ExchangeData.GetSymbolsAsync(default), false, "data.symbols");
+            await RunAndCheckResult(warnings, client => client.SpotApi.ExchangeData.GetSymbolNamesAsync(default), false, "data.symbols");
+            await RunAndCheckResult(warnings, client => client.SpotApi.ExchangeData.GetTickerAsync("ETH_USDT", default), false, "data");
+            await RunAndCheckResult(warnings, client => client.SpotApi.ExchangeData.GetTickersAsync(default), false, "data");
+            await RunAndCheckResult(warnings, client => client.SpotApi.ExchangeData.GetAssetDepositWithdrawInfoAsync(default, default), false, "data.currencies");
+            await RunAndCheckResult(warnings, client => client.SpotApi.ExchangeData.GetServerStatusAsync(default), false, "data.service");
+            await RunAndCheckResult(warnings, client => client.SpotApi.ExchangeData.GetKlinesAsync("ETH_USDT", Enums.KlineInterval.OneDay, default, default, default, default), false, "data");
+            await RunAndCheckResult(warnings, client => client.SpotApi.ExchangeData.GetKlineHistoryAsync("ETH_USDT", Enums.KlineInterval.OneDay, default, default, default, default), false, "data");
+            await RunAndCheckResult(warnings, client => client.SpotApi.ExchangeData.GetTradesAsync("ETH_USDT", default, default), false, "data");
+            await RunAndCheckResult(warnings, client => client.SpotApi.ExchangeData.GetOrderBookAsync("ETH_USDT", default, default), false, "data");
+            foreach (var warning in warnings)
+                Assert.Warn(warning.Message);
         }
 
         [Test]
         public async Task TestSpotTrading()
         {
-            await RunAndCheckResult(client => client.SpotApi.Trading.GetOpenOrdersAsync(default, default, default, default, default, default), true, true, "data");
-            await RunAndCheckResult(client => client.SpotApi.Trading.GetClosedOrdersAsync(default, default, default, default, default, default), true, true, "data");
-            await RunAndCheckResult(client => client.SpotApi.Trading.GetUserTradesAsync(default, default, default, default, default, default), true, true, "data");
+            var warnings = new List<Exception>();
+            await RunAndCheckResult(warnings, client => client.SpotApi.Trading.GetOpenOrdersAsync(default, default, default, default, default, default), true, "data");
+            await RunAndCheckResult(warnings, client => client.SpotApi.Trading.GetClosedOrdersAsync(default, default, default, default, default, default), true, "data");
+            await RunAndCheckResult(warnings, client => client.SpotApi.Trading.GetUserTradesAsync(default, default, default, default, default, default), true, "data");
+            foreach (var warning in warnings)
+                Assert.Warn(warning.Message);
         }
 
         [Test]
         public async Task TestUsdFuturesAccount()
         {
-            await RunAndCheckResult(client => client.UsdFuturesApi.Account.GetBalancesAsync(default), true, true, "data");
-            await RunAndCheckResult(client => client.UsdFuturesApi.Account.GetTransferHistoryAsync(default, default, default, default, default, default), true, true, "data.records");
+            var warnings = new List<Exception>();
+            await RunAndCheckResult(warnings, client => client.UsdFuturesApi.Account.GetBalancesAsync(default), true, "data");
+            await RunAndCheckResult(warnings, client => client.UsdFuturesApi.Account.GetTransferHistoryAsync(default, default, default, default, default, default), true, "data.records");
+            foreach (var warning in warnings)
+                Assert.Warn(warning.Message);
         }
 
         [Test]
         public async Task TestUsdFuturesExchangeData()
         {
-            await RunAndCheckResult(client => client.UsdFuturesApi.ExchangeData.GetContractsAsync(default, default), false, true, "data.symbols", ignoreProperties: ["tradfi_info"]);
-            await RunAndCheckResult(client => client.UsdFuturesApi.ExchangeData.GetOrderBookAsync("ETHUSDT", default), false, true, "data");
-            await RunAndCheckResult(client => client.UsdFuturesApi.ExchangeData.GetOpenInterestAsync("ETHUSDT", default), false, true, "data");
-            await RunAndCheckResult(client => client.UsdFuturesApi.ExchangeData.GetCurrentFundingRateAsync("ETHUSDT", default), false, true, "data");
-            await RunAndCheckResult(client => client.UsdFuturesApi.ExchangeData.GetKlinesAsync("ETHUSDT", Enums.FuturesKlineInterval.OneDay, DateTime.UtcNow.AddDays(-3), DateTime.UtcNow.AddHours(-1), default), false, true, "data");
+            var warnings = new List<Exception>();
+            await RunAndCheckResult(warnings, client => client.UsdFuturesApi.ExchangeData.GetContractsAsync(default, default), false, "data.symbols", ignoreProperties: ["tradfi_info"]);
+            await RunAndCheckResult(warnings, client => client.UsdFuturesApi.ExchangeData.GetOrderBookAsync("ETHUSDT", default), false, "data");
+            await RunAndCheckResult(warnings, client => client.UsdFuturesApi.ExchangeData.GetOpenInterestAsync("ETHUSDT", default), false, "data");
+            await RunAndCheckResult(warnings, client => client.UsdFuturesApi.ExchangeData.GetCurrentFundingRateAsync("ETHUSDT", default), false, "data");
+            await RunAndCheckResult(warnings, client => client.UsdFuturesApi.ExchangeData.GetKlinesAsync("ETHUSDT", Enums.FuturesKlineInterval.OneDay, DateTime.UtcNow.AddDays(-3), DateTime.UtcNow.AddHours(-1), default), false, "data");
+            foreach (var warning in warnings)
+                Assert.Warn(warning.Message);
         }
 
         [Test]
         public async Task TestUsdFuturesTrading()
         {
-            await RunAndCheckResult(client => client.UsdFuturesApi.Trading.GetClosedOrdersAsync("ETHUSDT", default, default, default, default, default), true, true, "data");
-            await RunAndCheckResult(client => client.UsdFuturesApi.Trading.GetOpenOrdersAsync(default, default, default, default, default), true, true, "data");
-            await RunAndCheckResult(client => client.UsdFuturesApi.Trading.GetTriggerOrdersAsync(default, default, default, default, default), true, true, "data");
-            await RunAndCheckResult(client => client.UsdFuturesApi.Trading.GetPositionsAsync(default, default), true, true, "data");
-            await RunAndCheckResult(client => client.UsdFuturesApi.Trading.GetPositionRiskAsync(default, default), true, true, "data", ignoreProperties: ["account"]);
-            await RunAndCheckResult(client => client.UsdFuturesApi.Trading.GetUserTradesAsync("ETHUSDT", default, default, default, default, default), true, true, "data");
+            var warnings = new List<Exception>();
+            await RunAndCheckResult(warnings, client => client.UsdFuturesApi.Trading.GetClosedOrdersAsync("ETHUSDT", default, default, default, default, default), true, "data");
+            await RunAndCheckResult(warnings, client => client.UsdFuturesApi.Trading.GetOpenOrdersAsync(default, default, default, default, default), true, "data");
+            await RunAndCheckResult(warnings, client => client.UsdFuturesApi.Trading.GetTriggerOrdersAsync(default, default, default, default, default), true, "data");
+            await RunAndCheckResult(warnings, client => client.UsdFuturesApi.Trading.GetPositionsAsync(default, default), true, "data");
+            await RunAndCheckResult(warnings, client => client.UsdFuturesApi.Trading.GetPositionRiskAsync(default, default), true, "data", ignoreProperties: ["account"]);
+            await RunAndCheckResult(warnings, client => client.UsdFuturesApi.Trading.GetUserTradesAsync("ETHUSDT", default, default, default, default, default), true, "data");
+            foreach (var warning in warnings)
+                Assert.Warn(warning.Message);
         }
 
         [Test]
